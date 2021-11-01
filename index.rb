20489c84617295d0a0efebbd0116d8ea38f36ef1
Will <machineboy2045@gmail.com>
2021-11-01 17:30:41 -0600

    init

---
+++ index.rb
@@ -0,0 +1,18 @@
+require 'json'
+require 'aws-sdk-dynamodb'
+
+def get_table_names(dynamodb_client)
+  result = dynamodb_client.list_tables
+  result.table_names
+rescue StandardError => e
+  puts "Error getting table names: #{e.message}"
+  'Error'
+end
+
+def lambda_handler(event:, context:)
+  region = 'us-east-2'
+  dynamodb_client = Aws::DynamoDB::Client.new(region: region)
+  result = dynamodb_client.scan(table_name: 'http-crud-tutorial-items')
+
+  { statusCode: 200, body: result.items }
+end
