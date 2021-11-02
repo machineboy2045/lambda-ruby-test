405f6f89d067f736618a6b392c6469fa45d3bf64
Will <machineboy2045@gmail.com>
2021-11-01 18:19:49 -0600

    simple

--- lambda_function.rb
+++ lambda_function.rb
@@ -1,17 +1,8 @@
 require 'json'
 require 'aws-sdk-dynamodb'
 
-def get_table_names(dynamodb_client)
-  result = dynamodb_client.list_tables
-  result.table_names
-rescue StandardError => e
-  puts "Error getting table names: #{e.message}"
-  'Error'
-end
-
 def lambda_handler(event:, context:)
-  region = 'us-east-2'
-  dynamodb_client = Aws::DynamoDB::Client.new(region: region)
+  dynamodb_client = Aws::DynamoDB::Client.new(region: 'us-east-2')
   result = dynamodb_client.scan(table_name: 'http-crud-tutorial-items')
 
   { statusCode: 200, body: JSON.generate(result.items) }
