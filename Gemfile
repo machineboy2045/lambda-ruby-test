be8e05b357f9d0be42fdc19320260854cbdcd0a5
Will <machineboy2045@gmail.com>
2021-11-01 21:08:41 -0600

    gemfile

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source 'https://rubygems.org' do
+  gem 'aws-record', '~> 2'
+end
--- lambda_function.rb
+++ lambda_function.rb
@@ -1,9 +1,15 @@
 require 'json'
-require 'aws-sdk-dynamodb'
+require 'aws-record'
+
+class UsersTable
+  include Aws::Record
+  set_table_name 'users'
+  string_attr :id, hash_key: true
+  string_attr :name
+end
 
 def lambda_handler(event:, context:)
-  dynamodb_client = Aws::DynamoDB::Client.new(region: 'us-east-2')
-  result = dynamodb_client.scan(table_name: 'http-crud-tutorial-items')
+  result = UsersTable.all
 
-  { statusCode: 200, body: JSON.generate(result.items) }
+  { statusCode: 200, body: JSON.generate(result) }
 end
