f0fad5169526c88f052e11d7e351f110e4c7efe5
Will <machineboy2045@gmail.com>
2021-11-02 16:46:06 -0600

    refactor

--- spec/lambda_function_spec.rb
+++ spec/lambda_function_spec.rb
@@ -1,13 +1,15 @@
 require './lambda_function'
 
 RSpec.describe "lambda_function" do
+  before do
+    migrate
+  end
 
   it "has db" do
     expect(dynamo_client.list_tables.table_names).to eq(['users'])
   end
 
   it "runs" do
-    migrate
     expect(lambda_handler.keys.sort).to eq([:body, :statusCode])
   end
 end
